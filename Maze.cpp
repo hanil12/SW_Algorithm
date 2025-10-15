430d5d36384bb6252fea10d7c10055710654d03a
Min SeungWon <zions09@kookmin.ac.kr>
2025-10-15 23:13:15 +0900

    row를 RED GREEM번갈아서 나오게 수정하기

---
+++ Maze.cpp
@@ -0,0 +1,47 @@
+#include "framework.h"
+#include "Maze.h"
+
+Maze::Maze()
+{
+	_blocks.resize(MAX_Y);
+
+	Vector offset = Vector(380,200);
+
+	for (int y = 0; y < MAX_Y; y++)
+	{
+		_blocks[y].reserve(MAX_X);
+		for (int x = 0; x < MAX_X; x++)
+		{
+			shared_ptr<Block> block = make_shared<Block>();
+			block->SetCenter(Vector(18*x, 18*y) + offset);
+			block->SetSize(Vector(13,13));
+			block->SetType((y % 2) == 0 ? Block::RED : Block::GREEN);
+			_blocks[y].push_back(block);
+		}
+	}
+}
+
+Maze::~Maze()
+{
+}
+
+void Maze::Update()
+{
+	for (auto blocks : _blocks)
+	{
+		for (auto block : blocks)
+		{
+			block->Update();
+			block->SetCenter(block->Center());
+		}
+	}
+}
+
+void Maze::Render(HDC hdc)
+{
+	for (auto blocks : _blocks)
+	{
+		for (auto block : blocks)
+			block->Render(hdc);
+	}
+}
